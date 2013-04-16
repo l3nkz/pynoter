4a8e37af16dc210cf3b3d3f0ee449d63c15eb7c8
Till Smejkal <till.smejkal+pynoter@ossmail.de>
2013-04-16 12:04:37 +0200

    Add documentation and description to debug file

--- _debug.py
+++ _debug.py
@@ -10,13 +10,26 @@
 
 import sys
 
+# global flag which indicates whether we want debug information or not.
 debug = False
 
 
 def debug_msg(message):
+    """
+    Method for displaying a debug message. This method will print the given
+    message to stdout if the debug flag is set.
+
+    @param message:     The debug message which should be displayed.
+    """
     if debug:
         print(message)
 
 
 def error_msg(message):
+    """
+    Method for displaying error messages. This method will print the given
+    message to stderr even if the debug flag is not set.
+
+    @param message:     The error message which should be displayed.
+    """
     sys.stderr.write(message)
