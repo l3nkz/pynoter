8e720a4f5ee90c9deb45a0401da17676e7faaed3
Till Smejkal <till.smejkal+pynoter@ossmail.de>
2015-02-05 15:08:28 +0100

    Add installation script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name="pynoter",
+    version="1.0",
+    description="Package for advanced desktop notifications",
+    author="Till Smejkal",
+    author_email="till.smejkal+pynoter@ossmail.de",
+    packages=['pynoter', 'pynoter.server']
+)
