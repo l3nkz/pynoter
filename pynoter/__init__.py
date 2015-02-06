820edf390fcb22b29d2fb15b92e14f165d6c915d
Till Smejkal <till.smejkal+pynoter@ossmail.de>
2015-02-06 11:49:05 +0100

    Update pynoter module file with description

--- pynoter/__init__.py
+++ pynoter/__init__.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
+################################################################################
+# pynoter
+#
+# This is the main module file. Just importing this will provide access to the
+# Server and the Client classes.
+#
+# License: GPLv3
+#
+# (c) Till Smejkal - till.smejkal+pynoter@ossmail.de
+################################################################################
+
 from pynoter.client import Client
 from pynoter.server import Server
 
+
 __all__ = ['Client', 'Server']
+
